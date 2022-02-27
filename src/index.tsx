ced4ff65ba41e0192918b56e92268b9d8348ccb6
Ilya758 <subscriber6436@gmail.com>
2022-02-27 19:51:49 +0300

    feat: implemented global styles and browser-router wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { GlobalStyle } from './components/Main/global/global.style';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
