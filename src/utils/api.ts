1a3e284b67a31b4afb91c778bb9ee05f2254a6e7
Ilya758 <subscriber6436@gmail.com>
2022-02-28 12:33:11 +0300

    fix: fixed utility methods api with generic-types

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,16 @@
 import { API_URL } from '../constants/apiData';
 
-export const getAllCategories = async () => {
+export const getAllCategories = async <T>(): Promise<T> => {
   const response = await fetch(`${API_URL}categories.php`);
-  return response.json();
+  return response.json() as Promise<T>;
 };
 
-export const getMealCategory = async (category: string) => {
+export const getMealCategory = async <T>(category: string): Promise<T> => {
   const response = await fetch(`${API_URL}filter.php?c=${category}`);
-  return response.json();
+  return response.json() as Promise<T>;
 };
 
-export const getMealById = async (mealId: string) => {
+export const getMealById = async <T>(mealId: string): Promise<T> => {
   const response = await fetch(`${API_URL}lookup.php?i=${mealId}`);
-  return response.json();
+  return response.json() as Promise<T>;
 };
