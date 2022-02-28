e4296d04a7c6faa7a9bd468a67444ffeefaf0b34
Ilya758 <subscriber6436@gmail.com>
2022-02-28 14:36:57 +0300

    feat: implemented context AppContext

---
+++ src/global/context/context.types.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IReducerState } from '../../constants/initialState.types';
+import { IAction } from '../../utils/reducer/reducer.types';
+
+export type TAppContext = {
+  state: IReducerState;
+  dispatch: React.Dispatch<IAction>;
+};
